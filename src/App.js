f5b97b9441811116c7c74f72db65d698032dadb0
rishu.singh <rishu.singh@thoughtworks.com>
2022-06-30 15:07:28 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="">shopcart</a>
+      </header>
+      <main>products</main>
+      <footer>contact</footer>
+    </div>
+  )
+}
+
+export default App
