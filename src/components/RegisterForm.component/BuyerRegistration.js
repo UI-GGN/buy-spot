59be25077e5d398d1f78f84316a53e91da6f136f
devsingh05298 <devsingh@thoughtworks.com>
2022-07-18 15:01:46 +0530

    [Dev]| updating test cases for Buyer Registration

--- src/components/RegisterForm.component/BuyerRegistration.js
+++ src/components/RegisterForm.component/BuyerRegistration.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import './Form.css'
-import {toast} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import { Alert } from 'react-bootstrap';
 
 export const BuyerRegistration = () => {
-  toast.configure()
   const {
     register,
     handleSubmit,
@@ -22,24 +19,23 @@ export const BuyerRegistration = () => {
     console.log(data)
   }
   const notify = ()=>{
- 
-   alert("Registered Successfully");
+   Alert("Registered Successfully");
 }
 
   return (
     <div className="register-form">
       <form onSubmit={handleSubmit(onSubmit)}>
-        <h3>Seller Registration</h3>
+        <h3>Buyer Registration</h3>
 
         <div className="form-input-email">
           <label>Email address</label>
           <input
             type="email"
             placeholder="Enter email"
+             data-testid="email"
             {...register('email', {
               required: 'Email is Required',
-              inputProps:{ "data-testid": "email" },
-
+             
               pattern: {
                 value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
                 message: 'Invalid email address',
@@ -58,12 +54,14 @@ export const BuyerRegistration = () => {
           <input
             type="password"
             placeholder="Enter Password"
-            inputProps={ {"data-testid": "email" }}
+            data-testid="password"
             {...register('password', {
               required: 'Password is Required',
+              inputProps:{ "data-testid": "password" },
               minLength: {
                 value: 6,
                 message: 'Password need to be 6 characters',
+
               },
             })}
             onKeyUp={() => {
@@ -78,16 +76,13 @@ export const BuyerRegistration = () => {
           <input
             type="password"
             placeholder="Confirm Password"
+            data-testid="confirmpassword"
             {...register('confirmPassword', {
               required: 'Password is Required',
               validate: {
                 confirmPassword: (value) =>
                   value === getValues().password || 'Passwords do not match!',
               },
-              // minLength: {
-              //   value: 6,
-              //   message: 'Password need to be 6 characters',
-              // },
             })}
             onKeyUp={() => {
               trigger('confirmPassword')
@@ -96,7 +91,7 @@ export const BuyerRegistration = () => {
           {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
         </div>
 
-        <button variant="primary" type="submit" onclick={notify}>
+        <button variant="primary" type="submit" onClick={notify}>
           Submit
         </button>
       </form>
--- src/components/RegisterForm.component/BuyerRegistration.spec.js
+++ src/components/RegisterForm.component/BuyerRegistration.spec.js
@@ -1,58 +1,90 @@
-import React from "react";
-import { fireEvent, render } from "@testing-library/react";
-import BuyerRegistration from "/Components/RegisterForm.components/BuyerRegistration";
-import { when } from "jest-when";
+import {render,screen} from '@testing-library/react'
+import { fireEvent, waitFor } from "@testing-library/react";
+import { BuyerRegistration } from './BuyerRegistration'
+//import {when} from "jest-when"
 
-jest.mock("moment");
+ it('should render Buyer registration page',async () => {
+render(<BuyerRegistration/>)
+const headingElement = screen.getByText(/Buyer Registration/i);
+expect(headingElement).toBeInTheDocument();
 
-describe("Basic rendering and functionality", () => {
-
-    const userInfoToBeRegistered = {
-        email: 'user1@gmail.com',
-        password: 'Sprite@26',
-        confirmPassword: 'Sprite@26'
-
-    };
-    const handleRegister = jest.fn();
-
-    const testHistory = { push: jest.fn() };
-    it("Should call buyerregistration api to create Buyer account on register", async () => {
-
-        const { getByTestId } = render(<BuyerRegistration values={userInfoToBeRegistered} history={testHistory} />);
-        var returnedPayload = when(handleRegister).calledWith({
-            email: 'user1@gmail.com',
-            password: 'Sprite@26',
-            confirmPasswor: 'Sprite@26',
-           
-
-        })
-            .mockResolvedValue("");
-
-        console.log(returnedPayload['Object']);
-        returnedPayload = returnedPayload;
-
-
-        fireEvent.change(getByTestId("email"), {
-            target: {
-                value: "user1@gmail.com"
-            }
-        });
-
-
-        fireEvent.change(getByTestId("password"), {
-            target: {
-                value: "Sprite@26"
-            }
-        });
-        fireEvent.change(getByTestId("confirmPassword"), {
-            target: {
-                value: "Sprite@26"
-            }
-        });
-
-        fireEvent.click(getByTestId("registerButton"));
-
-        await expect(returnedPayload).toBeTruthy();
-
-    });
 })
+
+it('should get textbox role in registration form',async () => {
+    render(<BuyerRegistration/>)
+    const formElement = screen.getByRole("textbox");
+    expect(formElement).toBeInTheDocument();
+    
+    })
+
+    it('should get by placeholdertext',async () => {
+        render(<BuyerRegistration/>)
+        const formElement = screen.getByPlaceholderText("Enter email");
+        expect(formElement).toBeInTheDocument();
+        
+        })
+
+        it('should get by testID',async () => {
+            render(<BuyerRegistration/>)
+            const emailElement = screen.getByTestId("email");
+            expect(emailElement).toBeInTheDocument();
+            
+            })
+        
+
+            {/* Testing using mock  */}
+            {/*
+
+            describe("basic rendering and functionality", ()=>{
+                it("Should call register service api to create account on register", async () => {
+                    const userInfoToBeRegistered={
+                        email:'xyz@gmail.com',
+                        password:'Abcd@1234',
+                        confirmpassword:'Abcd@1234'
+                    };
+                    const handleRegister = jest.fn();
+                    const testHistory={push:jest.fn()};
+
+                    const { getByTestId } = render(<BuyerRegistration values={userInfoToBeRegistered} history={testHistory} />);
+                    var returnedPayload = when(handleSubmit(onsubmit)).calledWith({
+                        email: 'xyz@gmail.com',
+                        password: 'Abcd@1234',
+                        reenterPassword: 'Abcd@1234'
+            
+                    })
+                        .mockResolvedValue("");
+            
+                    console.log(returnedPayload['Object']);
+                    returnedPayload = returnedPayload;
+            
+            
+                    fireEvent.change(getByTestId("email"), {
+                        target: {
+                            value: "xyz@gmail.com"
+                        }
+                    });
+            
+                    fireEvent.change(getByTestId("password"), {
+                        target: {
+                            value: "Abcd@1234"
+                        }
+                    });
+                    fireEvent.change(getByTestId("confirmPassword"), {
+                        target: {
+                            value: "Abcd@1234"
+                        }
+                    });
+            
+                    fireEvent.click(getByTestId("registerButton"));
+            
+                    await expect(returnedPayload).toBeTruthy();
+            
+                });
+            })
+        */}
+
+            
+
+
+    
+    
