73bacd606076d2791dc31496259b89d3f8152f99
rishu.singh <rishu.singh@thoughtworks.com>
2022-07-26 17:13:49 +0530

    [product-pg]|Rishu|Added routing to product page after login

--- src/components/product-list-component/Product.js
+++ src/components/product-list-component/Product.js
@@ -1,30 +1,46 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React from 'react'
+import { useSelector } from 'react-redux'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faIndianRupee } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
+import './Product.css'
 
 const Product = (props) => {
-
   const showPrice = useSelector((state) => {
     return state.loggedInUser
   })
+  const showProductPage = useSelector((state) => {
+    return state.loggedInUser
+  })
+
   return (
     <div className="col-md-4 custom-product" data-testid={'product'}>
-      <img className="img" src={props.image} alt="products" />
-      <br className="img-gap"></br>
-      <div className="product-label">
-        <div className="category">{props.category}</div>
-
-
-        {showPrice ? 
-          <div className="price">
-            <FontAwesomeIcon icon={faIndianRupee} className="ruppee" />
-            {props.price}
-          </div> : <div></div>}
-
-      </div>
+      {showProductPage ? (
+        <Link to="/product-page" className="product-page-link">
+          <div
+            className="col-md-4 custom-product"
+            data-testid={'product'}
+          ></div>
+          <img src={props.image} alt="products" className="img" />
+          <div className="product-label">
+            <div className="category">{props.category}</div>
+            <div className="price">
+              <FontAwesomeIcon icon={faIndianRupee} className="ruppee" />
+              {props.price}
+            </div>
+          </div>
+        </Link>
+      ) : (
+        <div className="col-md-4 custom-product" data-testid={'product'}>
+          <img className="img" src={props.image} alt="products" />
+          <br className="img-gap"></br>
+          <div className="product-label">
+            <div className="category">{props.category}</div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
 
-export default Product;
+export default Product
