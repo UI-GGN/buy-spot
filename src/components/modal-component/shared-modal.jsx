79827b4ce6e8adf5ffd07ab1f4eaf5013839fae7
shubhneetkaur <shubhneet.kaur@thoughtworks.com>
2022-08-03 18:51:35 +0530

    [Shubh] Style modal

--- src/components/modal-component/shared-modal.jsx
+++ src/components/modal-component/shared-modal.jsx
@@ -3,6 +3,7 @@ import Modal from "react-bootstrap/Modal";
 import React from "react";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
+import './shared-modal.css'
 
 export const SharedModal = (props) => {
   const property = props.property;
@@ -10,14 +11,14 @@ export const SharedModal = (props) => {
 
   return (
     <Modal className="shared-modal" show={props.show} onHide={handleCloseModal}>
-      <Modal.Header className=" border-0" closeButton>
+      <Modal.Header className="modal-header border-0" closeButton >
         <Modal.Title>{property}</Modal.Title>
       </Modal.Header>
-      <Modal.Body>You want to {property} as</Modal.Body>
-      <Modal.Footer className="modal-footer border-0">
+      <Modal.Body className="modal-body">You want to {property} as</Modal.Body>
+      <Modal.Footer className="modal-footer">
         <Button variant="light" onClick={handleCloseModal} data-testid="buyerButton" >
           <Link
-            style={{ textDecoration: "none", color: "black" }}
+            style={{ textDecoration: "none", color: "inherit" }}
             to={property === "Login" ? "/buyer-login" : "/buyer-registration"}
           >
             Buyer
@@ -25,7 +26,7 @@ export const SharedModal = (props) => {
         </Button>
         <Button variant="light" onClick={handleCloseModal} data-testid="sellerButton">
           <Link
-            style={{ textDecoration: "none", color: "black" }}
+            style={{ textDecoration: "none", color: "inherit" }}
             to={property === "Login" ? "/seller-login" : "/seller-registration"}
           >
             Seller
