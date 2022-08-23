176617308bc3ed3bb316739220777c11bbfeb084
dev05298 <devsingh05298@gmail.com>
2022-08-23 11:20:24 +0530

    [Dev]| Added PropTypes for Rating

--- src/components/product-list-component/product-description/Rating.jsx
+++ src/components/product-list-component/product-description/Rating.jsx
@@ -2,21 +2,20 @@ import React from 'react';
 import {AiFillStar} from "react-icons/ai";
 import  PropTypes  from 'prop-types'
 
-const Rating = props => {
-    const maxRating = props.maxRating
+const Rating = (props) => {
+    const maxRating=props.maxRating
+    
     const renderStar = () =>{
         const startsToRender = [];
-        for(let i=0;i < Math.round(maxRating) ; i++)
+        for(let i=0;i < Math.round(maxRating); i++)
         {
             startsToRender.push(<AiFillStar  />)
         }
         return startsToRender
     }
-
     Rating.propTypes = {
         maxRating: PropTypes.number.isRequired
     }
-    
     return(
         <>
         {renderStar()}
