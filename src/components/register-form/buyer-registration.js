f990147ab0d08102cf4baec087a032e806a6b063
Subhasree J <subhasree.j@thoughtworks.com>
2022-09-07 21:05:58 +0530

    Revert "i3feedback | Subhasree | Add navigation from registration page and changed background color of form"
    This reverts commit 58c66b36c078c11975e2950267e6f28adece988a.

--- src/components/register-form/buyer-registration.js
+++ src/components/register-form/buyer-registration.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import validation from './validation'
 import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom';
 import './form.css'
+
+import { Link } from 'react-router-dom'
 
 export const BuyerRegistration = () => {
   const [userDetails, setUserDetails] = useState({
@@ -11,17 +12,16 @@ export const BuyerRegistration = () => {
     confirmPassword: null,
     phoneNumber: null,
     role: "buyer"
-  });
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const [errors, setErrors] = useState({});
-  const [isSubmit, setIsSubmit] = useState(false);
+  })
+
+  const [errors, setErrors] = useState({})
+
   const [touched, setTouchedFields] = useState({
     email: false,
     password: false,
     confirmPassword: false,
     phoneNumber: false
-  });
+  })
 
   const resetInputFields = () => {
     setTouchedFields({
@@ -38,22 +38,27 @@ export const BuyerRegistration = () => {
     })
   };
 
+
   const handleChange = (e) => {
     setUserDetails({
       ...userDetails,
       [e.target.name]: e.target.value,
     })
-  };
-
+  }
   const handleValidations = (e) => {
     setTouchedFields({
       ...touched, [e.target.name]: true,
     })
     setErrors(validation(userDetails, e.target['name']))
-  };
+  }
+
+  const dispatch = useDispatch()
+
+  const [isSubmit, setIsSubmit] = useState(false)
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
+
     dispatch({
       type: 'REGISTER',
       payload: {
@@ -65,94 +70,108 @@ export const BuyerRegistration = () => {
         role:"buyer"
       },
     })
-    setIsSubmit(true);
-    navigate('/');
+    setIsSubmit(true)
     resetInputFields();
-  };
+  }
 
   return (
     <div className="register-form">
+      {Object.keys(errors).length === 0 && isSubmit ? (
+        <div className="success-page">
+          <h3>Successfully Registered</h3>
+          <Link
+            style={{ textDecoration: 'none', color: 'white' }}
+            to="/buyer-login"
+            className="success-page-link"
+          >
+            Login as Buyer
+          </Link>
+        </div>
+      ) : (
         <form onSubmit={handleSubmit}>
           <h3>Buyer Registration</h3>
-            <div className="form-inputs">
-              <label>Email address</label>
-              <input
-                type="email"
-                name="email"
-                value={userDetails.email}
-                onChange={handleChange}
-                placeholder="Enter email"
-                onKeyUp={handleChange}
-                autoComplete = 'off'
-                onBlur={handleValidations}
-              />
-              <div style={{ height: '36px' }}>
-                {touched.email && errors.email && <p>{errors.email}</p>}
-              </div>
+
+          <div className="form-inputs">
+            <label>Email address</label>
+            <input
+              type="email"
+              name="email"
+              value={userDetails.email}
+              onChange={handleChange}
+              placeholder="Enter email"
+              onKeyUp={handleChange}
+              onBlur={handleValidations}
+
+            />
+            <div style={{ height: '36px' }}>
+              {touched.email && errors.email && <p>{errors.email}</p>}
             </div>
-            <div className="form-inputs">
-              <label>Password</label>
-              <input
-                type="password"
-                name="password"
-                value={userDetails.password}
-                onChange={handleChange}
-                placeholder="Enter Password"
-                onKeyUp={handleChange}
-                autoComplete = 'off'
-                onBlur={handleValidations}
-              />{' '}
-              <div style={{ height: '36px' }}>
-                {touched.password && errors.password && <p>{errors.password}</p>}
-              </div>
+          </div>
+
+          <div className="form-inputs">
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              value={userDetails.password}
+              onChange={handleChange}
+              placeholder="Enter Password"
+              onKeyUp={handleChange}
+              onBlur={handleValidations}
+
+            />{' '}
+            <div style={{ height: '36px' }}>
+              {touched.password && errors.password && <p>{errors.password}</p>}
             </div>
-            <div className="form-inputs">
-              <label>Confirm Password</label>
-              <input
-                type="password"
-                name="confirmPassword"
-                value={userDetails.confirmPassword}
-                onChange={handleChange}
-                placeholder="Confirm Password"
-                onKeyUp={handleChange}
-                autoComplete = 'off'
-                onBlur={handleValidations}
-              />{' '}
-              <div style={{ height: '36px' }}>
-                {touched.confirmPassword && errors.confirmPassword && <p>{errors.confirmPassword}</p>}
-              </div>
+          </div>
+
+          <div className="form-inputs">
+            <label>Confirm Password</label>
+            <input
+              type="password"
+              name="confirmPassword"
+              value={userDetails.confirmPassword}
+              onChange={handleChange}
+              placeholder="Confirm Password"
+              onKeyUp={handleChange}
+              onBlur={handleValidations}
+            />{' '}
+            <div style={{ height: '36px' }}>
+              {touched.confirmPassword && errors.confirmPassword && <p>{errors.confirmPassword}</p>}
             </div>
-            <div className="form-inputs">
-              <label>Phone Number</label>
-              <input
-                type="number"
-                name="phoneNumber"
-                value={userDetails.phoneNumber}
-                onChange={handleChange}
-                placeholder="Phone Number"
-                onKeyUp={handleChange}
-                autoComplete = 'off'
-                onBlur={handleValidations}
-              />{' '}
-              <div style={{ height: '36px' }}>
-                {touched.phoneNumber && errors.phoneNumber && <p>{errors.phoneNumber}</p>}
-              </div>
+          </div>
+
+          <div className="form-inputs">
+            <label>Phone Number</label>
+            <input
+              type="number"
+              name="phoneNumber"
+              value={userDetails.phoneNumber}
+              onChange={handleChange}
+              placeholder="Phone Number"
+              onKeyUp={handleChange}
+              onBlur={handleValidations}
+            />{' '}
+            <div style={{ height: '36px' }}>
+              {touched.phoneNumber && errors.phoneNumber && <p>{errors.phoneNumber}</p>}
             </div>
-            <button
-              variant="primary"
-              type="submit"
-              disabled={
-                Object.keys(errors).length !== 0 ||
-                !userDetails.email ||
-                !userDetails.password ||
-                !userDetails.confirmPassword ||
-                !userDetails.phoneNumber
-              }
-            >
-              Submit
-            </button>
-            { isSubmit ? (handleSubmit) : ('') }
+          </div>
+
+          <button
+            variant="primary"
+            type="submit"
+            disabled={
+              Object.keys(errors).length !== 0 ||
+              !userDetails.email ||
+              !userDetails.password ||
+              !userDetails.confirmPassword ||
+              !userDetails.phoneNumber
+            }
+          >
+            Submit
+          </button>
         </form>
+      )}
     </div>
   )
 }
