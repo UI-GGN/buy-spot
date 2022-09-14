f316ea3989ccd52bcec26693a40b448d4b5b5ec8
shubhneetkaur <shubhneet.kaur@thoughtworks.com>
2022-09-14 18:30:00 +0530

    [Shubh] Reset search input when navigating back to homePage

--- src/App.js
+++ src/App.js
@@ -18,198 +18,176 @@ import { useEffect } from 'react';
 import { ProductDescription } from './components/product-list-component/product-description/product-description';
 
 function App() {
-    const [showRegister, setShowRegister] = useState(false);
-    const [showLogin, setShowLogin] = useState(false);
-    const [search, setSearch] = useState('');
-    const [productdata, setProducts] = useState([]);
-    const [filteredData, setFilteredData] = useState([]);
-    const [isSearched, setIsSearched] = useState(false);
+  const [showRegister, setShowRegister] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+  const [search, setSearch] = useState('');
+  const [productdata, setProducts] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [isSearched, setIsSearched] = useState(false);
 
-    const handleShowRegister = () => setShowRegister(true);
+  const handleShowRegister = () => setShowRegister(true);
 
-    const handleShowLogin = () => setShowLogin(true);
-    const dispatch = useDispatch();
+  const handleShowLogin = () => setShowLogin(true);
+  const dispatch = useDispatch();
 
-    const handleLoggedOut = () => {
-        dispatch({
-            type: 'LOGOUT',
-            payload: false,
-        });
-    };
-    const getProducts = async () => {
-        const response = await fetch('https://fakestoreapi.com/products');
-        setProducts(await response.json());
-    };
-
-    const filterProducts = () => {
-        const data = productdata.filter(
-            product =>
-                product.category.includes(search) ||
-                product.title.includes(search),
-        );
-        setFilteredData(data);
-    };
-
-    useEffect(() => {
-        getProducts();
-    }, []);
-
-    const setValue = () => {
-        setIsSearched(true);
-        const searchInput = document.getElementById('searchInput').value;
-        setSearch(searchInput.toLowerCase());
-        filterProducts();
-    };
-
-    const onClickHome = () => {
-        setIsSearched(false);
-    };
-
-    const userLoggedIn = useSelector(state => {
-        return state.loggedInUser;
+  const handleLoggedOut = () => {
+    dispatch({
+      type: 'LOGOUT',
+      payload: false,
     });
+  };
+  const getProducts = async () => {
+    const response = await fetch('https://fakestoreapi.com/products');
+    setProducts(await response.json());
+  };
 
-    const userEmail = useSelector(state => {
-        return state.users.map(user => user.email);
-    });
-
-    return (
-        <BrowserRouter>
-            <div className="app">
-                <Navbar style={{ backgroundColor: 'black' }} expand="lg">
-                    <Container>
-                        <Navbar.Brand
-                            style={{ color: 'white' }}
-                            as={Link}
-                            to="/"
-                            onClick={onClickHome}
-                        >
-                            BuySpot
-                        </Navbar.Brand>
-                        <input
-                            className="search-input"
-                            id="searchInput"
-                            type="text"
-                            placeholder="Search....."
-                        />
-                        <button
-                            className="search-button"
-                            type="submit"
-                            onClick={setValue}
-                        >
-                            Search
-                        </button>
-                        <Navbar.Toggle
-                            aria-controls="basic-navbar-nav"
-                            style={{
-                                backgroundColor: 'rgb(255,255,255,0.5)',
-                                height: '2.3rem',
-                            }}
-                        />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="ms-auto">
-                                <Nav.Link
-                                    style={{ color: 'white' }}
-                                    as={Link}
-                                    to="/"
-                                    onClick={onClickHome}
-                                >
-                                    Home
-                                </Nav.Link>
-                                <Nav.Link
-                                    style={{ color: 'white' }}
-                                    as={Link}
-                                    to="/about"
-                                >
-                                    About
-                                </Nav.Link>
-                                {userLoggedIn ? (
-                                    <NavDropdown
-                                        title={
-                                            'Hi ' +
-                                            userEmail.pop().split('@').shift()
-                                        }
-                                        id="basic-nav-dropdown"
-                                    >
-                                        <NavDropdown.Item
-                                            onClick={handleLoggedOut}
-                                        >
-                                            Logout
-                                        </NavDropdown.Item>
-                                    </NavDropdown>
-                                ) : (
-                                    <div style={{ display: 'flex' }}>
-                                        <Nav.Link
-                                            style={{ color: 'white' }}
-                                            onClick={handleShowRegister}
-                                        >
-                                            Register
-                                        </Nav.Link>
-
-                                        <Nav.Link
-                                            style={{ color: 'white' }}
-                                            onClick={handleShowLogin}
-                                        >
-                                            Login
-                                        </Nav.Link>
-                                    </div>
-                                )}
-
-                                <SharedModal
-                                    show={showLogin}
-                                    setShow={setShowLogin}
-                                    property={'Login'}
-                                />
-                                <SharedModal
-                                    show={showRegister}
-                                    setShow={setShowRegister}
-                                    property={'Register'}
-                                />
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-                <div>
-                    <Routes>
-                        <Route path="/about" element={<About />}></Route>
-                        <Route
-                            path="/"
-                            element={
-                                <Home
-                                    productData={
-                                        search === ''
-                                            ? productdata
-                                            : filteredData
-                                    }
-                                    isSearch={isSearched}
-                                />
-                            }
-                        ></Route>
-                        <Route
-                            path="/buyer-registration"
-                            element={<BuyerRegistration />}
-                        ></Route>
-                        <Route
-                            path="/seller-registration"
-                            element={<SellerRegistration />}
-                        ></Route>
-                        <Route
-                            path="/buyer-login"
-                            element={<BuyerLogin />}
-                        ></Route>
-                        <Route
-                            path="/seller-login"
-                            element={<SellerLogin />}
-                        ></Route>
-                        <Route
-                            path="/product/:productId"
-                            element={<ProductDescription />}
-                        ></Route>
-                    </Routes>
-                </div>
-            </div>
-        </BrowserRouter>
+  const filterProducts = () => {
+    const data = productdata.filter(
+      product =>
+        product.category.toLowerCase().includes(search) ||
+        product.title.toLowerCase().includes(search),
     );
+    setFilteredData(data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const setValue = () => {
+    setIsSearched(true);
+    const searchInput = document.getElementById('searchInput').value;
+    setSearch(searchInput.toLowerCase());
+    filterProducts();
+  };
+
+  const onClickHome = () => {
+    setIsSearched(false);
+    document.getElementById('searchInput').setAttribute('', '');
+  };
+
+  const userLoggedIn = useSelector(state => {
+    return state.loggedInUser;
+  });
+
+  const userEmail = useSelector(state => {
+    return state.users.map(user => user.email);
+  });
+
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <Navbar style={{ backgroundColor: 'black' }} expand="lg">
+          <Container>
+            <Navbar.Brand
+              style={{ color: 'white' }}
+              as={Link}
+              to="/"
+              onClick={onClickHome}
+            >
+              BuySpot
+            </Navbar.Brand>
+            <input
+              className="search-input"
+              id="searchInput"
+              type="text"
+              placeholder="Search....."
+            />
+            <button className="search-button" type="submit" onClick={setValue}>
+              Search
+            </button>
+            <Navbar.Toggle
+              aria-controls="basic-navbar-nav"
+              style={{
+                backgroundColor: 'rgb(255,255,255,0.5)',
+                height: '2.3rem',
+              }}
+            />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Nav.Link
+                  style={{ color: 'white' }}
+                  as={Link}
+                  to="/"
+                  onClick={onClickHome}
+                >
+                  Home
+                </Nav.Link>
+                <Nav.Link style={{ color: 'white' }} as={Link} to="/about">
+                  About
+                </Nav.Link>
+                {userLoggedIn ? (
+                  <NavDropdown
+                    title={'Hi ' + userEmail.pop().split('@').shift()}
+                    id="basic-nav-dropdown"
+                  >
+                    <NavDropdown.Item onClick={handleLoggedOut}>
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                ) : (
+                  <div style={{ display: 'flex' }}>
+                    <Nav.Link
+                      style={{ color: 'white' }}
+                      onClick={handleShowRegister}
+                    >
+                      Register
+                    </Nav.Link>
+
+                    <Nav.Link
+                      style={{ color: 'white' }}
+                      onClick={handleShowLogin}
+                    >
+                      Login
+                    </Nav.Link>
+                  </div>
+                )}
+
+                <SharedModal
+                  show={showLogin}
+                  setShow={setShowLogin}
+                  property={'Login'}
+                />
+                <SharedModal
+                  show={showRegister}
+                  setShow={setShowRegister}
+                  property={'Register'}
+                />
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <div>
+          <Routes>
+            <Route path="/about" element={<About />}></Route>
+            <Route
+              path="/"
+              element={
+                <Home
+                  productData={isSearched ? filteredData : productdata}
+                  isSearch={isSearched}
+                />
+              }
+            ></Route>
+            <Route
+              path="/buyer-registration"
+              element={<BuyerRegistration />}
+            ></Route>
+            <Route
+              path="/seller-registration"
+              element={<SellerRegistration />}
+            ></Route>
+            <Route path="/buyer-login" element={<BuyerLogin />}></Route>
+            <Route path="/seller-login" element={<SellerLogin />}></Route>
+            <Route
+              path="/product/:productId"
+              element={<ProductDescription />}
+            ></Route>
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
