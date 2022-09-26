037d6c728c64939cddb7596ea7fba14ca8f11e31
Subhasree J <subhasree.j@thoughtworks.com>
2022-09-26 19:23:49 +0530

    techDebt| Subhasree | Add localstorage and fix formatting errors

--- src/App.js
+++ src/App.js
@@ -48,11 +48,6 @@ function App() {
         );
         setFilteredData(data);
     };
-
-    useEffect(() => {
-        getProducts();
-    }, []);
-
     const setValue = () => {
         const searchInput = document.getElementById('searchInput').value;
         setSearch(searchInput.toLowerCase());
@@ -62,6 +57,14 @@ function App() {
     const userLoggedIn = useSelector(state => {
         return state.loggedInUser;
     });
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('userDetails', userLoggedIn);
+    }, [userLoggedIn]);
 
     const userEmail = useSelector(state => {
         return state.users.map(user => user.email);
